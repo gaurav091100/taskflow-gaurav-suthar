cc31cf4394044d5ba5a4b2ca8b08a078d3f39c4b
gaurav091100 <gaurav091100@gmail.com>
2026-04-12 02:09:02 +0530

    chore: remove json-server and express setup after switching to MSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { worker } from "./mocks/browser";
 
+worker.start();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
---
+++ src/mocks/handlers.js
@@ -0,0 +1,98 @@
+import { http, HttpResponse } from "msw";
+
+let users = [
+  {
+    id: "1",
+    name: "Test User",
+    email: "test@example.com",
+    password: "password123",
+  },
+];
+
+let projects = [
+  {
+    id: "1",
+    name: "Demo Project",
+    description: "Test project",
+    owner_id: "1",
+    created_at: "2026-04-01",
+  },
+];
+
+export const handlers = [
+  // LOGIN
+  http.post("http://localhost:4000/auth/login", async ({ request }) => {
+    const body = await request.json();
+
+    const user = users.find(
+      (u) => u.email === body.email && u.password === body.password
+    );
+
+    if (!user) {
+      return HttpResponse.json(
+        { error: "unauthorized" },
+        { status: 401 }
+      );
+    }
+
+    return HttpResponse.json({
+      token: "mock-token",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  }),
+
+  // REGISTER
+  http.post("http://localhost:4000/auth/register", async ({ request }) => {
+    const body = await request.json();
+
+    if (!body.name || !body.email || !body.password) {
+      return HttpResponse.json(
+        {
+          error: "validation failed",
+          fields: {
+            name: !body.name ? "is required" : undefined,
+            email: !body.email ? "is required" : undefined,
+            password: !body.password ? "is required" : undefined,
+          },
+        },
+        { status: 400 }
+      );
+    }
+
+    const exists = users.find((u) => u.email === body.email);
+    if (exists) {
+      return HttpResponse.json(
+        {
+          error: "validation failed",
+          fields: { email: "user already exists" },
+        },
+        { status: 400 }
+      );
+    }
+
+    const newUser = {
+      id: crypto.randomUUID(),
+      ...body,
+    };
+
+    users.push(newUser);
+
+    return HttpResponse.json(
+      {
+        token: "mock-token",
+        user: {
+          id: newUser.id,
+          name: newUser.name,
+          email: newUser.email,
+        },
+      },
+      { status: 201 }
+    );
+  }),
+
+  
+];
